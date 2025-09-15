be9bcbe6a584cae4e8649f69182706cc3df4a7f5
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-09-15 09:54:32 -0300

    Feat: adicionas valores

--- queries/01-insert-artists.sql
+++ queries/01-insert-artists.sql
@@ -10,3 +10,13 @@ CREATE TABLE ARTISTAS (
     ATIVO BOOLEAN DEFAULT TRUE
 );
 
+INSERT INTO ARTISTAS(NOME, TIPO, MEMBROS, GENERO_MUSICAL, PAIS_ORIGEM, ANO_INICIO, MUSICA_MAIS_OUVIDA, ATIVO) VALUES 
+('Coldplay', 'Banda', '4', 'Rock Alternativo', 'Reino Unido', 1997, 'Viva La Vida', TRUE),
+('Taylor Swift', 'Solo', 'Taylor Swift', 'Pop', 'Estados Unidos', 2006, 'Cruel Summer', TRUE),
+('Anitta', 'Solo', 'Anitta', 'Pop', 'Brasil', 2010, 'Envolver', TRUE),
+('Queen', 'Banda', '4', 'Rock', 'Reino Unido', 1970, 'Bohemian Rhapsody', FALSE),
+('Ed Sheeran', 'Solo', 'Ed Sheeran', 'Pop', 'Reino Unido', 2004, 'Shape of You', TRUE),
+('Imagine Dragons', 'Banda', '4', 'Pop Rock', 'Estados Unidos', 2008, 'Radioactive', TRUE),
+('Billie Eilish', 'Solo', 'Billie Eilish', 'Indie Pop', 'Estados Unidos', 2015, 'bad guy', TRUE),
+('Linkin Park', 'Banda', '6', 'Rock', 'Estados Unidos', 1996, 'In the End', FALSE),
+('The Beatles', 'Banda', '4', 'Rock', 'Reino Unido', 1960, 'Here Comes the Sun', FALSE);
