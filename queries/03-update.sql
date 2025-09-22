84df49f253019dffefba79685efebae6717201d5
Felipe Antunes Campos <felipe.a.campos7@aluno.senai.br>
2025-09-22 14:39:22 -0300

    Feat: comandos iniciasi de update

---
+++ queries/03-update.sql
@@ -0,0 +1,12 @@
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET username = 'ProGamer2025'
+WHERE username = 'NoobMaster';
+
+UPDATE usuarios SET pontos = pontos + 1000;
+
+SELECT * FROM usuarios;
+
+UPDATE usuarios SET email = 'main@gmail.com'
+WHERE email = 'master@email.com';
+
